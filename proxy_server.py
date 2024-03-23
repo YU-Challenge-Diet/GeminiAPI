3398067cfd98439545ec3b2d48f43ad5dfae4759
albertjacobsz <dekater403@gmail.com>
2024-03-23 17:42:29 +0800

    fix

--- proxy_server.py
+++ proxy_server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify, Response
 import requests
 
+import http.client
+import typing
+import urllib.request
+
+from vertexai.generative_models import GenerativeModel, Image
 app = Flask(__name__)
 
 
@@ -14,25 +19,36 @@ def upload_file():
     text = request.form['text']
 
     # Validate the file extension
-    if image.filename.split('.')[-1].lower() not in ['jpeg', 'jpg', 'png', 'heic']:
+    if image.filename.split('.')[-1].lower() not in ['jpg', 'png', 'heic']:
         return jsonify({"error": "Invalid image format"}), 400
 
     # Prepare the files and data to be forwarded
     files = {'image': (image.filename, image.read())}
     data = {'text': text}
-    print(f"got REQ, file: and data:{data}")
+
     # Forward the request to the second server
     try:
         # Assuming the second server is expecting the image as a multipart/form-data
         # and the text as form data
-        response = requests.post(
-            'http://172.0.0.1:80/forward', files=files, data=data)
+        # Pass multimodal prompt
+        model = GenerativeModel("gemini-1.0-pro-vision")
+        response = model.generate_content(
+            [
+                image.read(),
+                text
+
+            ]
+        )
+        print(response)
         response.raise_for_status()
         # Forward the second server's response back to the initial client
         return Response(response.content, status=response.status_code, content_type=response.headers['Content-Type'])
     except requests.exceptions.RequestException as e:
         # Handle any errors that occur during the forwarding process
         return jsonify({"error": str(e)}), 500
+
+
+# Load images from Cloud Storage URI
 
 
 if __name__ == '__main__':
